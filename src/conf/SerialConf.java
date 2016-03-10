9053bfdf1816b53b1bd8af6cf32df4e481520073
Swing Leo <lishiyun19@gmail.com>
2016-03-10 15:52:28 +0800

    配置

--- src/conf/SerialConf.java
+++ src/conf/SerialConf.java
@@ -1,5 +1,35 @@
 package conf;
 
 public class SerialConf {
-	public static final String PORT = "/dev/ttyS0";
+	/**
+	 * Windows 串口.
+	 */
+	public static final String WINDOWS_PORT = "COM3";
+	
+	/**
+	 * Linux 串口.
+	 */
+	public static final String LINUX_PORT = "/dev/ttyS0";
+	
+	/**
+	 * 波特率.
+	 */
+	public static final int BAUD = 38400;
+	
+	/**
+	 * 超时.
+	 */
+	public static final int TIME_OUT = 5000;
+	
+	/**
+	 * 缓存空间.
+	 */
+	public static final int BUFFER_SIZE = 4096;
+	
+	/**
+	 * 服务器.
+	 */
+	public static final String URL = "http://localhost";
+	
+	
 }
